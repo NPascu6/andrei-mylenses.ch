084dc68f1fa3233040bac2b401fa79c24f2b9a6f
Norbert Pascu <58431719+NPascu6@users.noreply.github.com>
2023-10-13 20:40:29 +0200

    Added hide / show description.

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import PhotoCard from './PhotoCard';
+import CollapseIcon from './CollapseIcon';
+import ExpandIcon from './ExpandIcon';
 
 
 interface PhotoGalleryProps {
@@ -10,10 +12,16 @@ interface PhotoGalleryProps {
 
 const PhotoGallery = ({ images, imageDescriptions }: PhotoGalleryProps) => {
     const [selectedImage, setSelectedImage] = useState<any | null>(null);
+    const [isCardVisible, setIsCardVisible] = useState<boolean>(false);
 
     const getTitle = (image: any) => {
         const title = image.split('/').pop()?.split('.')[0];
         return title?.replace(/-/g, ' ');
+    }
+
+    const handleExpand = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        e.stopPropagation();
+        setIsCardVisible(!isCardVisible);
     }
 
 
@@ -35,7 +43,10 @@ const PhotoGallery = ({ images, imageDescriptions }: PhotoGalleryProps) => {
             {selectedImage && (
                 <div
                     className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-90"
-                    onClick={() => setSelectedImage(null)}
+                    onClick={() => {
+                        setSelectedImage(null)
+                        setIsCardVisible(false)
+                    }}
                 >
                     <div className="rounded-lg shadow-lg p-4 flex flex-col md:flex-row">
                         <img
@@ -43,7 +54,17 @@ const PhotoGallery = ({ images, imageDescriptions }: PhotoGalleryProps) => {
                             alt={selectedImage}
                             className="max-h-full max-w-full"
                         />
-                        <PhotoCard title={getTitle(selectedImage)} description={imageDescriptions.find(d => d.title === getTitle(selectedImage))?.description ?? ""} price={10} onAddToBasket={() => null} /></div>
+                        {isCardVisible ? <div className='fixed top-0 right-0 '>
+                            <div className='fixed top-10 right-4 bg-black'>
+                                <div onClick={(e) => handleExpand(e)}><CollapseIcon /></div>
+                            </div>
+                            <PhotoCard title={getTitle(selectedImage)} description={imageDescriptions.find(d => d.title === getTitle(selectedImage))?.description ?? ""} price={10} onAddToBasket={() => null} />
+                        </div> :
+                            <div className='fixed top-10 right-4 border-2 border-gray-200 bg-black'>
+                                <div onClick={(e) => handleExpand(e)}><ExpandIcon /></div>
+                            </div>
+                        }
+                    </div>
                 </div>
             )}
         </div>
