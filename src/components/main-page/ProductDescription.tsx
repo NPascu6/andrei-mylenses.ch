ec1baae92ce03ddd8f713a73b8e58945c7103cc3
Norbert Pascu <58431719+NPascu6@users.noreply.github.com>
2023-10-31 16:17:38 +0100

    Fixed picfair.

--- src/components/main-page/ProductDescription.tsx
+++ src/components/main-page/ProductDescription.tsx
@@ -126,10 +126,12 @@ const ProductDescription = () => {
                     </span>
                 </p>
                 <hr className="border-t-2 m-4" />
-                <p className="text-xl  font-bold  mb-4 ">
+                <p className="text-xl font-bold">
                     Also you can discover the world through the lens of a professional photographer. I offer high-quality digital copies of photos for sale online.
                     <span className="flex justify-center m-3 pointer">
-                        <a href="https://www.flickr.com/photos/andrei_ro/" rel="noreferrer" target="_blank" className="flex justify-center"><img loading="lazy" className="  w-1/3" alt='flikr' src="https://logowik.com/content/uploads/images/330_flickr.jpg" /></a>
+                        <a href="https://thelensofandrei.picfair.com" rel="noreferrer" target="_blank" className="flex justify-center ">
+                            <img style={{ maxWidth: '25%' }} alt="picfair" src="https://logowik.com/content/uploads/images/picfair3077.logowik.com.webp" />
+                        </a>
                     </span>
                 </p>
                 <hr className="border-t-2" />
